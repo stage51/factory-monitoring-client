bc2af873014a8057336d4824be6b4495baf0617f
merkulovandrej <stg51@hotmail.com>
2024-11-05 10:50:13 +0300

    fix date picker

--- components/shared/admin/table/columns.tsx
+++ components/shared/admin/table/columns.tsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import { ColumnDef } from "@tanstack/react-table";
-import { DateRange } from "react-day-picker";
-import { MoreHorizontal } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import {
     Dialog,
     DialogContent,
@@ -13,144 +9,143 @@ import {
     DialogTrigger,
     DialogClose
 } from "@/components/ui/dialog";
-import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { DatePicker } from "../../date-picker";
+import { DateRange } from "react-day-picker";
 
 interface Props<HeadersTypes> {
     headers: Array<{ key: keyof HeadersTypes; label: string }>;
     editable?: boolean;
 }
 
+function EditDialog<HeadersTypes>({ data, headers }: { data: HeadersTypes; headers: Array<{ key: keyof HeadersTypes; label: string }> }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleDelete = () => {
+        setIsOpen(false);
+        console.log("Deleted item:", data);
+    };
+
+    return (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+            <DialogTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Открыть диалог</span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[600px]">
+                <DialogHeader className="gap-2">
+                    <DialogTitle>Редактирование</DialogTitle>
+                    <DialogDescription>Редактирование элемента</DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    {headers.map(({ key, label }) =>
+                        key !== "id" ? (
+                            <div key={key.toString()} className="grid grid-cols-4 items-center gap-4">
+                                <Label htmlFor={key.toString()} className="text-right">
+                                    {label}
+                                </Label>
+                                {data[key] instanceof Date ? (
+                                    <DatePicker
+                                        value={data[key] as Date} 
+                                        className="col-span-3 w-full" 
+                                    />
+                                ) : (
+                                    <>
+                                        {typeof data[key] === "boolean" ? (
+                                            <Select>
+                                                <SelectTrigger className="col-span-3">
+                                                    <SelectValue placeholder={data[key] ? "Да" : "Нет"} />
+                                                </SelectTrigger>
+                                                <SelectContent>
+                                                    <SelectItem value="true">Да</SelectItem>
+                                                    <SelectItem value="false">Нет</SelectItem>
+                                                </SelectContent>
+                                            </Select>
+                                        ) : (
+                                            <Input id={key.toString()} defaultValue={String(data[key])} className="col-span-3" />
+                                        )}
+                                    </>
+                                )}
+                            </div>
+                        ) : null
+                    )}
+                </div>
+                <DialogFooter className="justify-between gap-2">
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button variant="destructive">Удалить</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[600px]">
+                            <DialogHeader className="gap-2">
+                                <DialogTitle>Удаление</DialogTitle>
+                                <DialogDescription>Вы действительно хотите удалить элемент?</DialogDescription>
+                            </DialogHeader>
+                            <DialogFooter className="justify-between gap-2">
+                                <DialogClose asChild>
+                                    <Button variant="destructive" onClick={handleDelete}>Удалить</Button>
+                                </DialogClose>
+                                <DialogClose asChild>
+                                    <Button variant="outline">Отмена</Button>
+                                </DialogClose>
+                            </DialogFooter>
+                        </DialogContent>
+                    </Dialog>
+                    <DialogClose asChild>
+                        <Button variant="default">Сохранить</Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+
 export default function generateColumns<HeadersTypes>({ headers, editable = true }: Props<HeadersTypes>) {
-    const columns: ColumnDef<HeadersTypes>[] = headers.map(({ key, label }) => ({
+    const columns = headers.map(({ key, label }) => ({
+        id: key,
         accessorKey: key,
         header: label,
         cell: ({ row }) => {
             const original = row.original as HeadersTypes;
             const value = original[key];
-
-            if (typeof value === "boolean") {
-                return value ? "Да" : "Нет";
-            } else if (value instanceof Date) {
-                return value.toLocaleString();
-            } else {
-                return value;
-            }
+            if (typeof value === "boolean") return value ? "Да" : "Нет";
+            if (value instanceof Date) return value.toLocaleString();
+            return value;
         },
         filterFn: (row, columnId, filterValue) => {
             const value = row.getValue(columnId);
             if (value instanceof Date) {
-                const date = value as Date;
                 const { from, to } = filterValue as DateRange;
-                return (!from || date >= from) && (!to || date <= to);
+                return (!from || value >= from) && (!to || value <= to);
             } else if (typeof value === "string") {
-                const filterStr = filterValue as string;
-                return filterStr ? value.toLowerCase().includes(filterStr.toLowerCase()) : true;
+                return filterValue ? value.toLowerCase().includes(filterValue.toLowerCase()) : true;
             }
             return false;
         },
     }));
-    editable && columns.push({
-        id: "actions",
-        accessorKey: "acrions",
-        header: "Действия",
-        cell: ({ row }) => {
-            const data = row.original as HeadersTypes;
-            const [isOpen, setIsOpen] = useState(false);
 
-            const handleDelete = () => {
-                setIsOpen(false);
-                console.log("Deleted item:", data);
-            };
-
-            return (
-                <Dialog open={isOpen} onOpenChange={setIsOpen}>
-                    <DialogTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0" onClick={() => setIsOpen(true)}>
-                            <span className="sr-only">Открыть диалог</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[600px]">
-                        <DialogHeader className="gap-2">
-                            <DialogTitle>Редактирование</DialogTitle>
-                            <DialogDescription>
-                                Редактирование элемента
-                            </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                            {headers.map(({ key, label }) =>
-                                key !== "id" ? (
-                                    <div key={key.toString()} className="grid grid-cols-4 items-center gap-4">
-                                        <Label htmlFor={key.toString()} className="text-right">
-                                            {label}
-                                        </Label>
-                                        {data[key] instanceof Date ? (
-                                            <DatePicker
-                                                value={data[key]}
-                                                className="col-span-3 w-full"
-                                            />
-                                        ) : (<>
-                                            {typeof data[key] === "boolean" ? (
-                                                <Select>
-                                                    <SelectTrigger className="col-span-3">
-                                                        <SelectValue placeholder={data[key] == true ? ("Да") : ("Нет")} />
-                                                    </SelectTrigger>
-                                                    <SelectContent>
-                                                        <SelectItem value="true">Да</SelectItem>
-                                                        <SelectItem value="false">Нет</SelectItem>
-                                                    </SelectContent>
-                                                </Select>
-                                            ) :
-                                            (<Input
-                                                id={key.toString()}
-                                                defaultValue={String(data[key])}
-                                                className="col-span-3"
-                                            />)}
-                                        </>)}
-                                    </div>
-                                ) : null
-                            )}
-                        </div>
-                        <DialogFooter className="justify-between gap-2">
-                            <Dialog>
-                                <DialogTrigger asChild>
-                                    <Button variant="destructive">Удалить</Button>
-                                </DialogTrigger>
-                                <DialogContent className="sm:max-w-[600px]">
-                                    <DialogHeader className="gap-2">
-                                        <DialogTitle>Удаление</DialogTitle>
-                                        <DialogDescription>
-                                            Вы действительно хотите удалить элемент?
-                                        </DialogDescription>
-                                    </DialogHeader >
-                                    <DialogFooter className="justify-between gap-2">
-                                        <DialogClose asChild>
-                                            <Button variant="destructive" type="submit" onClick={handleDelete}>Удалить</Button>
-                                        </DialogClose>
-                                        <DialogClose asChild>
-                                            <Button variant="outline">Отмена</Button>
-                                        </DialogClose>
-                                    </DialogFooter>
-                                </DialogContent>
-                            </Dialog>
-                            <DialogClose asChild>
-                                <Button variant="default" type="submit">Сохранить</Button>
-                            </DialogClose>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
-            );
-        },
-    });
+    if (editable) {
+        columns.push({
+            id: "actions",
+            accessorKey: "actions",
+            header: "Действия",
+            cell: ({ row }) => (
+                <EditDialog 
+                    data={row.original as HeadersTypes} 
+                    headers={headers}
+                    onChange={(key, value) => {
+                        row.original[key] = value;
+                    }}
+                />
+            ) as HeadersTypes[keyof HeadersTypes],
+        });
+    }
 
     return columns;
 }
--- components/shared/date-picker.tsx
+++ components/shared/date-picker.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
@@ -14,47 +13,52 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
-interface DatePickerProps{
-    value: Date | undefined;
-    onChange?: (date: Date | undefined) => void;
-    className?: string;
-    placeholder?: string;
+interface DatePickerProps {
+  value: Date | undefined;
+  className?: string;
+  placeholder?: string;
 }
 
-export function DatePicker({value, onChange, className, placeholder = "Выберите дату"} : DatePickerProps) {
+export function DatePicker({
+  value,
+  className,
+  placeholder = "Выберите дату",
+}: DatePickerProps) {
   const [date, setDate] = React.useState<Date>()
-    React.useEffect(() => {
-        setDate(value);
-    }, [value]);
-    const handleDateChange = (newDate: Date | undefined) => {
-        setDate(newDate);
-    };
+
+  React.useEffect(() => {
+    setDate(value);
+  }, [value]);
+
+  const handleDateChange = (newDate: Date | undefined) => {
+    setDate(newDate);
+  };
 
   return (
     <div className={cn("grid gap-2", className)}>
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button
-          variant={"outline"}
-          className={cn(
-            "w-auto justify-start text-left font-normal",
-            !date && "text-muted-foreground"
-          )}
-        >
-          <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "LLL dd, y") : <span>{placeholder}</span>}
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-auto p-0">
-        <Calendar
+      <Popover modal>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-auto justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "LLL dd, y") : <span>{placeholder}</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
             mode="single"
             defaultMonth={date}
             selected={date}
             onSelect={handleDateChange}
             initialFocus
-        />
-      </PopoverContent>
-    </Popover>
+          />
+        </PopoverContent>
+      </Popover>
     </div>
   )
 }
